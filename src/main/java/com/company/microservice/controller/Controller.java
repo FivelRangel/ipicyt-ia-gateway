1ff7b0be948319ca5c3e0a160180f6ca974a8c9e
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-04-20 17:04:50 -0600

    Update Controller.java

--- src/main/java/com/company/microservice/controller/Controller.java
+++ src/main/java/com/company/microservice/controller/Controller.java
@@ -12,5 +12,14 @@ public class Controller {
         return ResponseEntity.ok("Hello from Railway + Spring!");
     }
 
+     @PostMapping("/medicion")
+    public ResponseEntity<Medicion> registrarMedicion(@RequestBody Medicion medicion) {
+        Medicion guardada = medicionRepository.save(medicion);
+        return ResponseEntity.ok(guardada);
+    }
 
+    @GetMapping("/mediciones")
+    public ResponseEntity<List<Medicion>> obtenerTodas() {
+        return ResponseEntity.ok(medicionRepository.findAll());
+    }
 }
