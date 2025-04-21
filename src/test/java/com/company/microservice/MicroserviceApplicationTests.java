1efb08a3e07cba2438bfc20279f7793fcae08919
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-04-20 18:17:50 -0600

    Update MicroserviceApplicationTests.java

--- src/test/java/com/company/microservice/MicroserviceApplicationTests.java
+++ src/test/java/com/company/microservice/MicroserviceApplicationTests.java
@@ -2,12 +2,13 @@ package com.company.microservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("railway") // 🔥 ESTA LÍNEA ACTIVA TU `application-railway.yml`
 class MicroserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
