4dc9bdedfe7d4abd871f7a3cb981512c45baf311
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-04-28 23:01:50 -0600

    Add files via upload

---
+++ src/main/java/com/company/microservice/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.company.microservice.config; // <- Ajusta este package si es diferente en tu proyecto
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://ipicyt-dashboard-react-plus-production.up.railway.app", // tu frontend en Railway
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
